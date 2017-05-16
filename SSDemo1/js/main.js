de8605e768398eadf06efb722a27cf4ca86bb670
mihaelaetr <mishetoetr@gmail.com>
2017-05-16 15:18:15 +0300

    Add files via upload

--- SSDemo1/js/main.js
+++ SSDemo1/js/main.js
@@ -1,9 +1,9 @@
 //lightbox 
-function openModal() {
+function openGallery() {
   document.getElementById('myModal').style.display = "block";
 }
 
-function closeModal() {
+function closeGallery() {
   document.getElementById('myModal').style.display = "none";
 }
 
@@ -18,20 +18,14 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
 
 //map
